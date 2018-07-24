ba23488179edfe97faf683f07830fc42421ac60d
Leo Maroni <leo@labcode.de>
2018-07-24 14:41:08 +0200

    abschluss summen

--- frontend/js/abschluss.js
+++ frontend/js/abschluss.js
@@ -52,7 +52,8 @@ $(function () {
             success: (data) => {
                 console.log(data);
                 if (data.transaktionen[0].bezahlen) {
-                    $('#ergebnis').html('<h5 class="center thin">Du musst bezahlen!</h5>' +
+                    $('#ergebnis').html('<h6 class="center">Soll/Ist/Differenz (' + data.sollAusgaben + ' &euro; / ' + data.istAusgaben + ' &euro; / ' + data.difference + '&euro;)</h6>' +
+                        '<h5 class="center thin">Du musst bezahlen!</h5>' +
                         '<table class="centered responsive-table0">' +
                         '                <thead>' +
                         '                    <tr>' +
@@ -63,7 +64,8 @@ $(function () {
                         '<tbody id="ergebnis-table"></tbody>' +
                         '            </table>')
                 } else {
-                    $('#ergebnis').html('<h5 class="center thin">Du bekommst Geld!</h5>' +
+                    $('#ergebnis').html('<h6 class="center">Soll/Ist/Differenz (' + data.sollAusgaben + ' &euro; / ' + data.istAusgaben + ' &euro; / ' + data.difference + '&euro;)</h6>' +
+                        '<h5 class="center thin">Du bekommst Geld!</h5>' +
                         '<table class="centered responsive-table0">' +
                         '                <thead>' +
                         '                    <tr>' +
