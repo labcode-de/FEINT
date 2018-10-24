53e6c5de087eb5945f72ad09e3dc6b8f36cd04fe
Leo Maroni <leo@labcode.de>
2018-10-24 21:57:44 +0200

    added isAuthenticatedAndAuthorizeEvents handler

--- backend/auth/isAuthenticated.js
+++ backend/auth/isAuthenticated.js
@@ -8,7 +8,11 @@ const isAuthenticated = (req, res, next) => {
     if (token) {
         try {
             const decoded = jwt.verify(token, auth_config.jwt_token.secret);
-            db.collection("users").findOne({service_id: decoded.user.service_id, _id: ObjectID(decoded.user.id), authenticatedServices: decoded.user.service}, (err, dbRes) => {
+            db.collection("users").findOne({
+                service_id: decoded.user.service_id,
+                _id: ObjectID(decoded.user.id),
+                authenticatedServices: decoded.user.service
+            }, (err, dbRes) => {
                 req.inspector = {
                     isAuthenticated: true,
                     JWTuser: decoded.user,
@@ -22,8 +26,66 @@ const isAuthenticated = (req, res, next) => {
     } else {
         return res.status(401).json({error: 'Invalid access token!'});
     }
-    if(error) {
+    if (error) {
         return res.status(401).json({error: 'Invalid access token!'});
     }
 }
 module.exports.isAuthenticated = isAuthenticated;
+const isAuthenticatedAndAuthorizedEvent = (req, res, next) => {
+    const token = req.headers['x-access-token'];
+    const eventId = req.params.eventId;
+    let error = false;
+    if (token) {
+        try {
+            const decoded = jwt.verify(token, auth_config.jwt_token.secret);
+            db.collection("users").findOne({
+                service_id: decoded.user.service_id,
+                _id: ObjectID(decoded.user.id),
+                authenticatedServices: decoded.user.service
+            }, (err, dbResUsers) => {
+                req.inspector = {
+                    isAuthenticated: true,
+                    JWTuser: decoded.user,
+                    user: dbResUsers
+                };
+                let userAllowedEvents = dbResUsers.allowedEvents;
+                let allowedEventIDs = [];
+                let allowedEventIDsString = []
+                for (const allowedEventI in userAllowedEvents) {
+                    if (userAllowedEvents.hasOwnProperty(allowedEventI)) {
+                        const allowedEvent = userAllowedEvents[allowedEventI];
+                        allowedEventIDs.push(allowedEvent.eventId);
+                        allowedEventIDsString.push(allowedEvent.eventId.toString());
+                    }
+                }
+                db.collection("events").findOne({'identifier': eventId}, (err, event) => {
+                    if(event !== null) {
+                        if (allowedEventIDsString.includes(event._id.toString())) {
+                            let allowedEvent = dbResUsers.allowedEvents[allowedEventIDsString.indexOf(event._id.toString())];
+                            allowedEvent.identifier = event.identifier;
+                            let allowedEventIdentifier = [];
+                            dbResUsers.allowedEvents.forEach((allowedEvent) => {
+                                allowedEventIdentifier.push(allowedEvent.identifier);
+                            });
+                            if (allowedEventIdentifier.includes(eventId)) {
+                                return next();
+                            } else {
+                                res.status(401).json({error: 'Not a allowed event for access token or no event is available with this identifier!'});
+                            }
+                        } else {
+                            res.status(401).json({error: 'Not a allowed event for access token or no event is available with this identifier!'});
+                        }
+                    } else {
+                        res.status(401).json({error: 'Not a allowed event for access token or no event is available with this identifier!'});
+                    }
+                });
+
+            });
+        } catch (err) {
+            res.status(401).json({error: 'Invalid access token!'});
+        }
+    } else {
+        return res.status(401).json({error: 'Invalid access token!'});
+    }
+}
+module.exports.isAuthenticatedAndAuthorizedEvent = isAuthenticatedAndAuthorizedEvent;
